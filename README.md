54914b21c507f7fb724bfe0043d2b9619af51e78
Evan Chisholm <elchisholm248@gmail.com>
2020-09-02 11:55:17 -0400

    added an event for when a new user joins a room

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Backend for my virtual whiteboard project
+
+it was created using node.js express and Socket.io
+You can check out the client side repo [here]()
--- index.js
+++ index.js
@@ -4,22 +4,28 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 require('dotenv').config();
 
-const users = ['user a', 'user b']
+const users = ['user a', 'user b'];
 
-app.get('/', (req, res) => res.send('it works'))
+// just a test route to see if the server is running
+app.get('/', (req, res) => res.send('it works'));
 
+// run this on connection to the websocket
 io.on('connection', socket => {
-    console.log('connected')
+    console.log('connected');
     socket.emit('current users', users);
+    // when oneperson draws send the information to everyone else using the same whiteboard
     socket.on('stroke', ({ lastPos, currentPos, roomId }) => {
         socket.to(roomId).emit('stroke', { lastPos, currentPos });
-    })
+    });
+    // when the person wants to connect to a different room switch their socket to that room
     socket.on('change id', id => {
-        console.log(id)
+        console.log(id);
         socket.join(id);
-    })
-})
+        socket.to(id).emit('new user');
+    });
+});
 
+// listen on port 4000 or whatever you put in your env
 http.listen(process.env.PORT || 4000, () => {
-    console.log('listening on port 4000')
+    console.log('listening on port 4000');
 });
